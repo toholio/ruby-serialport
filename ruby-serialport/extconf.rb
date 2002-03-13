fbecdbc7919d4cccc8fb9895fc683642894a7e24
moumar <moumar@6607fed9-16ac-4f31-92eb-e512782b99c2>
2002-03-13 12:47:12 +0000

    Working version
    git-svn-id: svn://rubyforge.org/var/svn/ruby-serialport/trunk@17 6607fed9-16ac-4f31-92eb-e512782b99c2

--- ruby-serialport/extconf.rb
+++ ruby-serialport/extconf.rb
@@ -1,9 +1,9 @@
 require 'mkmf'
 
+exit(1) if not have_header("termios.h") or not have_header("unistd.h")
 printf("checking for OS... ")
 STDOUT.flush
 os = /-([a-z]+)[0-9]*.*/.match(RUBY_PLATFORM)[1]
 puts(os)
 $CFLAGS += " -D#{os}"
 create_makefile("serialport")
-#with_config(debug)
